7ffec0377857d942f12adf593cf28db5735197f0
atsvirchkova <atsvirchkova@gmail.com>
2015-12-01 12:50:46 -0800

    Project created
    Project created

---
+++ src/com/example/android_taxi_booker/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.android_taxi_booker;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+	 @Override
+	   public void onCreate(Bundle savedInstanceState) {
+	      super.onCreate(savedInstanceState);
+	      setContentView(R.layout.activity_main);
+	   }
+	   
+	   @Override
+	   public boolean onCreateOptionsMenu(Menu menu) {
+	      getMenuInflater().inflate(R.menu.main, menu);
+	      return true;
+	   }
+}
