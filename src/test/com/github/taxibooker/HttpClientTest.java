c1c35549d5eb9305a88cf15d8016c9126d498b32
Tania Kakovska <tannia.zabelina@gmail.com>
2015-12-09 22:48:03 +0200

    Implemented my first JUnit TestCase

---
+++ src/test/com/github/taxibooker/HttpClientTest.java
@@ -0,0 +1,22 @@
+package test.com.github.taxibooker;
+
+import static org.junit.Assert.*;
+
+
+public class HttpClientTest {
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+        System.out.println("Hello from setUp!");
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+        System.out.println("Hello from tearDown!");
+    }
+
+    @org.junit.Test
+    public void testSomething() throws Exception {
+        System.out.println("Hello from testSomething!");
+    }
+}
